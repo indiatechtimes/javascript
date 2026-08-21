12948d08a2f346636060217907e0411a26dd3391
Your Name <you@example.com>
2026-08-21 23:57:29 +0530

    datatype

---
+++ dataType.js
@@ -0,0 +1,18 @@
+let name = "Ilya";
+console.log(`hello ${1}`);
+
+
+console.log(`hello ${"name"}`);
+
+console.log(`hello ${name}`);
+
+let isGreater = 4 > 1;
+console.log(isGreater);
+
+
+let age = null;
+console.log(age);
+
+
+let month
+console.log(month);
