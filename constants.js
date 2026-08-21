ab20346d2fb124b4f093308f3892734568349ca0
Your Name <you@example.com>
2026-08-21 23:40:26 +0530

    Add initial implementations for constants, let variables, and data types

---
+++ constants.js
@@ -0,0 +1,10 @@
+const color = "blue";
+console.log(color);
+
+// if i want to change the value of color?
+// color = "pink";
+// console.log(color); error 
+
+// so we cannot reassign the constant variable;
+
+
---
+++ let.js
@@ -0,0 +1,24 @@
+let a;
+a = "javascript";
+console.log(a);
+
+let name = "js";
+console.log(name);
+
+let name1 = "hello", age = 21, dep = "bca", access = true;
+console.log(`${name1},${age},${dep},${access}`);
+
+let name2;
+name2 = 44;
+console.log(name2);
+// now change the value of name2;
+name2 = 55;
+console.log(name2);
+
+
+
+let hello = "hello world";
+let message = hello;
+console.log(hello);
+console.log(message);
+
