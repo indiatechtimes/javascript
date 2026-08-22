9d0f1ed5d8a844c73230a6d140830cbb4483c355
Your Name <you@example.com>
2026-08-22 12:08:20 +0530

    Add initial implementations for prompt age check and type conversion

---
+++ prompt.js
@@ -0,0 +1,9 @@
+
+
+let age = prompt("your age?");
+
+if (age>18) {
+    console.log("you are admitted");
+} else {
+    console.log("not admitted");
+}
---
+++ typeconversion.js
@@ -0,0 +1,13 @@
+let value = true;
+console.log(typeof value);
+
+
+
+let val = true;
+console.log(val);
+console.log(typeof val);
+let v = String(val);
+console.log(v); // the output true is string!
+console.log(typeof v);
+
+
