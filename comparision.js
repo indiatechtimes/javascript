78d60d1365d63aad12a991bcb92babf27f5a474e
Your Name <you@example.com>
2026-08-29 12:09:39 +0530

    Add initial implementations for comparison and arithmetic operations

---
+++ comparision.js
@@ -0,0 +1,37 @@
+// console.log("Z" > "A");
+
+// console.log("Glow" > "Glee");
+
+// console.log("Bee" > "Be");
+
+// console.log("hello" > "world");
+
+// console.log("goto" > "GoOn");
+
+// console.log("apple" > "pineapple")//false
+
+// console.log("2" > "12");//true
+
+
+// let a = 0;
+// console.log(Boolean(a));// false
+// let b = "0";
+// console.log(Boolean(b)); // true
+
+// console.log(a == b); // true
+
+
+// console.log(0 == false);// true
+// console.log(0 === false); //false
+
+// console.log(0 == false);//true
+// console.log("" == false);// true
+// // but
+// console.log(0 === false);//false
+// console.log("" === false);//false
+
+
+
+console.log(undefined == null);// true
+
+console.log(undefined === null); //false
---
+++ operators.js
@@ -0,0 +1,17 @@
+let x = 1;
+console.log(x)
+x = -x;
+console.log(x);
+
+
+let a = 7;
+let b = 10;
+console.log(a - b);
+
+
+
+let s = 2, d = 10;
+console.log(d % s);
+
+
+console.log(5 ** 2);
